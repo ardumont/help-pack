06998e2b2de2f21e7f11568789672f4eb25b5a57
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-06 01:28:39 +0100

    help-pack: Move guide-key position to bottom

--- help-pack.el
+++ help-pack.el
@@ -4,16 +4,17 @@
 
 ;;; Code:
 
-(use-package guide-key
-  :config
-  (custom-set-variables
-   ;; Delay in seconds before guide
-   '(guide-key/idle-delay 0.8)
-   ;; key sequence to learn
-   '(guide-key/guide-key-sequence '("C-c" "C-x" "M-?" "#"))
-   '(guide-key/recursive-key-sequence-flag t))
-  ;; Enable guide-key-mode
-  (guide-key-mode 1))
+(require 'guide-key)
+(custom-set-variables
+ ;; Delay in seconds before guide
+ '(guide-key/idle-delay 0.8)
+ ;; key sequence to learn
+ '(guide-key/guide-key-sequence '("C-c" "C-x" "M-?" "#"))
+ '(guide-key/recursive-key-sequence-flag t)
+ '(guide-key/popup-window-position :bottom))
+
+;; Enable guide-key-mode
+(guide-key-mode 1)
 
 (provide 'help-pack)
 ;;; help-pack.el ends here
